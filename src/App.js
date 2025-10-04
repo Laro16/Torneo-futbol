014550d398813f1a466b8f1d2fcecb2de2968c19
Laro16 <luis21aro@gmail.com>
2025-10-04 02:43:01 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const initialNewsData = [];
 const STANDINGS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSfPgrAnVvcoHmUhsIWAw3RksYuqMfwwocIUQpga26AqlRyOcqWVFoit_haKgJ3d2FU9FoU6G2Swoao/pub?gid=0&single=true&output=csv';
 const SCORERS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSfPgrAnVvcoHmUhsIWAw3RksYuqMfwwocIUQpga26AqlRyOcqWVFoit_haKgJ3d2FU9FoU6G2Swoao/pub?gid=1191349899&single=true&output=csv';
 const NEWS_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSfPgrAnVvcoHmUhsIWAw3RksYuqMfwwocIUQpga26AqlRyOcqWVFoit_haKgJ3d2FU9FoU6G2Swoao/pub?gid=747825916&single=true&output=csv';
-const BRACKET_URL = 'https://docs.google.com/spreadsheets/d/1lewQR_Cr0ZtBBJpMs9UGGDr5h8xArjJ448j6ydRLV20/edit?gid=1728980058#gid=1728980058'; // <-- RECUERDA PEGAR TU LINK CSV DE LLAVES AQUI
+const BRACKET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSfPgrAnVvcoHmUhsIWAw3RksYuqMfwwocIUQpga26AqlRyOcqWVFoit_haKgJ3d2FU9FoU6G2Swoao/pub?output=csv'; // <-- RECUERDA PEGAR TU LINK CSV DE LLAVES AQUI
 
 // --- Gemini API Helper ---
 const callGeminiAPI = async (prompt) => {
